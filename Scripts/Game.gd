3e77ba36ec76da172498f76525daa9d3a82714d4
herroncm12 <59851837+herroncm12@users.noreply.github.com>
2020-04-28 06:43:57 -0400

    Add files via upload

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -3,6 +3,24 @@ extends Spatial
 func _ready():
 	pass
 
+# warning-ignore:unused_argument
 func _unhandled_input(event):
 	if Input.is_action_pressed("Quit"):
 		get_tree().quit()
+
+
+func _on_Timer_timeout():
+	pass # Replace with function body.
+
+
+# warning-ignore:unused_argument
+func _on_Nearby_area_exited(area):
+	pass # Replace with function body.
+
+
+func _on_Nearby_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_Nearby_body_exited(body):
+	pass # Replace with function body.
